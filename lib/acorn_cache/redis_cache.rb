ef12f86a55a52da5df502eafa0a047a370e5bd57
Vincent J. DeVendra <VinceDeVendra@gmail.com>
2016-02-28 19:24:26 -0500

    fix redis env variables in case no password is set up

--- lib/acorn_cache/redis_cache.rb
+++ lib/acorn_cache/redis_cache.rb
@@ -3,9 +3,14 @@ require 'redis'
 class Rack::AcornCache
   module RedisCache
     def self.redis
-      @redis ||= Redis.new(host: ENV["ACORNCACHE_HOST"],
-                           port: ENV["ACORNCACHE_PORT"].to_i,
-                           password: ENV["ACORNCACHE_PASSWORD"])
+      args = { host: ENV["ACORNCACHE_HOST"],
+               port: ENV["ACORNCACHE_PORT"].to_i }
+
+      if ENV["ACORNCACHE_REDIS_PASSWORD"]
+        args.merge!(password: ENV["ACORNCACHE_REDIS_PASSWORD"])
+      end
+
+      @redis ||= Redis.new(args)
     end
   end
 end
